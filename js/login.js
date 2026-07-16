8236b8cbd03a54849a5fd2db456e5d339091d795
Siti Hajar Binti Esa <ctarejar23@gmail.com>
2026-07-16 08:02:25 +0800

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function loginUser() {
+
+    let username = document.getElementById("username").value;
+
+    let password = document.getElementById("password").value;
+
+    let errorMessage = document.getElementById("errorMessage");
+
+    if (username === "admin" && password === "12345") {
+
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        errorMessage.innerHTML = "Invalid username or password";
+
+    }
+
+    return false;
+
+}
